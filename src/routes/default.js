53a21bf54590f06f202006e1668aa9d4ae17aded
sanchezzjf <joao.sanchezz@hotmail.com>
2021-11-16 19:38:18 -0300

    testing

--- src/routes/default.js
+++ src/routes/default.js
@@ -4,12 +4,12 @@ import { PostModel } from "../models/Post.js";
 const defaultRouter = new Router()
 
 defaultRouter.route('/')
-    .get((req, res) => {
+    .get((req, res, next) => {
     PostModel.find().then((post) => {
         res.render('home', {post: post})
     })
     .post((req, res) => {
-        
+
     })
 })
 
