b26252a5efacafddb8292bb4c2d8dd8766628812
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-01-22 09:06:06 -0300

    testing

--- src/routes/default.js
+++ src/routes/default.js
@@ -4,11 +4,17 @@ import { PostModel } from "../models/Post.js";
 const defaultRouter = new Router()
 
 defaultRouter.route('/')
-    .get((req, res) => {
+    .get((req, res, next) => {
     PostModel.find().then((post) => {
         res.render('home', {post: post})
+        })
     })
-})
+    .post((req, res, next) => {
 
+    })
+
+defaultRouter.get('/spotify', (req, res) => {
+    res.render('spotify/spotHome')
+})    
 
 export { defaultRouter }
