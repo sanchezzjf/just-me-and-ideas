4e884f244285b3a2ae978d262534f047fc5e40f8
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-01-24 12:29:13 -0300

    testing

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -1,15 +1,26 @@
 import { Router } from 'express';
+import { connected } from 'process';
 import { stringify } from 'querystring';
 import { logger } from '../util/logger.js';
 
 const spotRouter = new Router()
 
 const client_id = process.env.CLIENT_ID
+const scope = 'user-read-private user-read-email'
 const redirect_uri = process.env.REDIRECT_URI
 
 spotRouter.get('/', (req, res) => {
-    console.log(process.env.REDIRECT_URI)
-    res.render('spotify/spotHome', {client_id: `${client_id}`, redirect_uri: `${redirect_uri}`})
+    res.render('spotify/spotHome')
+})
+
+spotRouter.get('/login', (req, res) => {
+    res.redirect('https://accounts.spotify.com/authorize?' + 
+        stringify({
+            response_type: code,
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+        }))
 })
 
 spotRouter.get('/auth', (req, res) => {
