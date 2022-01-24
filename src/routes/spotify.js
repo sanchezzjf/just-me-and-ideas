4166c4a5073c48996dbab3dbef23a252d96f9054
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-01-24 12:21:52 -0300

    testing

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -4,12 +4,12 @@ import { logger } from '../util/logger.js';
 
 const spotRouter = new Router()
 
-const client_id = `${process.env.CLIENT_ID}`
-const redirect_uri = `${process.env.REDIRECT_URI}`
+const client_id = process.env.CLIENT_ID
+const redirect_uri = process.env.REDIRECT_URI
 
 spotRouter.get('/', (req, res) => {
     console.log(process.env.REDIRECT_URI)
-    res.render('spotify/spotHome', {client_id: client_id, redirect_uri: redirect_uri})
+    res.render('spotify/spotHome', {client_id: `${client_id}`, redirect_uri: `${redirect_uri}`})
 })
 
 spotRouter.get('/auth', (req, res) => {
