3ef7defa252dfbe06cd6eb04e89da16f717a098e
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-01-26 12:54:10 -0300

    testing

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -42,7 +42,7 @@ spotRouter.route('/auth')
                 logger.info(`${data.access_token}`)
                 const access_token = data.access_token
                 if(access_token){
-                    res.redirect('/spotify',200, {access_token: access_token})
+                    res.cookie(access_token)
                 }
             })
         })
