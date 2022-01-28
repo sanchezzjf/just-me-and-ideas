fab05bb7fd8fc7a5c5159c6bfa98f4b420815f7b
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-01-28 18:02:22 -0300

    testing

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -42,8 +42,9 @@ spotRouter.route('/auth')
             getAccessToken(authOptions, client_id, client_secret).then((data) => {
                 logger.info(`${data.access_token}`)
                 const access_token = data.access_token
+                const refresh_token = data.refresh_token
                 if(access_token){
-                    res.redirect('/spotify')
+                    res.redirect('/spotify' + stringify({access_token: access_token, refresh_token: refresh_token}))
                 }
             })
         })
