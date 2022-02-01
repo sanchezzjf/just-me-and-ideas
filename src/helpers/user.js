638f2f52c06ce5710718faf7ba892dea042f35b7
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-02-01 09:01:33 -0300

    testing

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { logger } from "../util/logger";
 
 const get_current_user = (access_token) => {
     axios.get('https://api.spotify.com/v1/me', {
@@ -6,9 +7,7 @@ const get_current_user = (access_token) => {
             'Authorization': `Bearer ${access_token}`,
             'Content-type': 'application/json'
         }
-    }).then((res) => {
-        res.data
-    })
+    }).then(res => res.data).catch(err => logger.error(err))
 }
 
 export { 
