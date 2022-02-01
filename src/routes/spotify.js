506d39efd2d2061e9ad1eca3a99a6696cd1ad4a8
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-02-01 17:21:58 -0300

    testing

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -67,7 +67,7 @@ spotRouter.route('/auth')
         
 
 spotRouter.post('/users/:command', (req, res) => {
-    logger.info(`${req.params.command}, ${req.body.access_token}`)
+    logger.info(`${req.params}, ${req.body.access_token}`)
     if(req.params.get_current_user){
         get_current_user(req.body.access_token).then((data) => {
             logger.info(data)
